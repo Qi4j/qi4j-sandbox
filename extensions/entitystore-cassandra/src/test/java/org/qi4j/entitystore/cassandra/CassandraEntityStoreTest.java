3e8a4619d5d374797ee451f721ced0a6137fcf38
Philippe Van Dyck <pvdyck@gmail.com>
2010-11-18 12:35:39 +0100

    Ignored test until the Hector version of the E/S is ready

--- extensions/entitystore-cassandra/src/test/java/org/qi4j/entitystore/cassandra/CassandraEntityStoreTest.java
+++ extensions/entitystore-cassandra/src/test/java/org/qi4j/entitystore/cassandra/CassandraEntityStoreTest.java
@@ -1,9 +1,14 @@
 package org.qi4j.entitystore.cassandra;
 
+import org.junit.Ignore;
 import org.qi4j.bootstrap.AssemblyException;
 import org.qi4j.bootstrap.ModuleAssembly;
 import org.qi4j.test.entity.AbstractEntityStoreTest;
 
+
+//need to launch a cassandra instance b4 running this test...
+//waiting for Hector version of the ES
+@Ignore
 public class CassandraEntityStoreTest extends AbstractEntityStoreTest {
 
     @Override
