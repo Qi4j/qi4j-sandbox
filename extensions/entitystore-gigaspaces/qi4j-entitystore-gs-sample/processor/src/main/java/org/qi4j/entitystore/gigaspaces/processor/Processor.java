c5da67d3b7cb7bc87fb06dd4f423444d7db68f50
Niclas Hedhman <niclas@hedhman.org>
2015-04-18 00:11:15 +0800

    Gigaspaces entitystore was laying around on my disk from somewhere. committing it, to not loose it, but it is probably for pre-1.0

---
+++ extensions/entitystore-gigaspaces/qi4j-entitystore-gs-sample/processor/src/main/java/org/qi4j/entitystore/gigaspaces/processor/Processor.java
@@ -0,0 +1,44 @@
+package org.qi4j.entitystore.gigaspaces.processor;
+
+import org.qi4j.entitystore.gigaspaces.common.Data;
+
+import org.openspaces.events.adapter.SpaceDataEvent;
+
+
+/**
+ * The processor simulates work done no un-processed Data object. The processData
+ * accepts a Data object, simulate work by sleeping, and then sets the processed
+ * flag to true and returns the processed Data.
+ */
+public class Processor {
+
+    private long workDuration = 100;
+
+    /**
+     * Sets the simulated work duration (in milliseconds). Default to 100.
+     */
+    public void setWorkDuration(long workDuration) {
+        this.workDuration = workDuration;
+    }
+
+    /**
+     * Process the given Data object and returning the processed Data.
+     *
+     * Can be invoked using OpenSpaces Events when a matching event
+     * occurs.
+     */
+    @SpaceDataEvent
+    public Data processData(Data data) {
+        // sleep to simulate some work
+        try {
+            Thread.sleep(workDuration);
+        } catch (InterruptedException e) {
+            // do nothing
+        }
+        data.setProcessed(true);
+        data.setData("PROCESSED : " + data.getRawData());
+        System.out.println(" ------ PROCESSED : " + data);
+        return data;
+    }
+
+}
